63d1ac23a80d855cfc324ede36e4140b760845c8
Aloys Augustin <aloys.augustin@polytechnique.org>
2017-08-30 21:46:53 +0200

    INST-75 Add erlang and rabbitmq in the package
    Credits to https://github.com/rabbitmq/erlang-rpm for the minimal erlang
    distribution required for RabbitMQ. Erlang takes 57MB in the installed
    packages (full distribution in ~150MB). RabbitMQ taks 6MB.
    
    The RabbitMQ installation is laid out to mimic the debian package setup.
    All binaries are symlinked into /opt/scalr-server/embedded/bin, and the
    actual files are in
    /opt/scalr-server/embedded/lib/rabbitmq/rabbitmq_server-<version>. This
    allows to ship two different versions in a package in the future.

---
+++ config/software/rabbitmq.rb
@@ -0,0 +1,38 @@
+name 'rabbitmq'
+default_version '3.6.11'
+
+dependency 'erlang'
+
+source url: "https://www.rabbitmq.com/releases/rabbitmq-server/v#{version}/rabbitmq-server-generic-unix-#{version}.tar.xz"
+relative_path "rabbitmq_server-#{version}"
+
+version '3.6.11' do
+  source md5: '62dccb5a1bcf8a97450c5796723fe8b9'
+end
+
+license 'MPL-2.0'
+license_file 'LICENSE'
+skip_transitive_dependency_licensing true
+
+
+build do
+  mkdir "#{install_dir}/embedded/lib/rabbitmq/bin"
+
+  # Remove docs
+  %w{share INSTALL}.each do |path|
+    delete "#{path}"
+  end
+
+  sync '.', "#{install_dir}/embedded/lib/rabbitmq/rabbitmq_server-#{version}"
+
+  # Roughly reproduce debian package structure
+  %w{rabbitmqctl rabbitmq-defaults rabbitmq-env rabbitmq-plugins rabbitmq-server}.each do |binary|
+    link "#{install_dir}/embedded/lib/rabbitmq/rabbitmq_server-#{version}/sbin/#{binary}", "#{install_dir}/embedded/lib/rabbitmq/bin/#{binary}"
+  end
+
+  %w{rabbitmqctl rabbitmq-defaults rabbitmq-env rabbitmq-plugins rabbitmq-server}.each do |binary|
+    link "#{install_dir}/embedded/lib/rabbitmq/bin/#{binary}", "#{install_dir}/embedded/bin/#{binary}"
+  end
+
+end
+
