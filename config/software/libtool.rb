bf28fcc91d1da5051c10e58398955197574ee5c1
Aloys Augustin <aloys.augustin@polytechnique.org>
2017-09-21 00:19:19 +0200

    Libtool licensing info

--- config/software/libtool.rb
+++ config/software/libtool.rb
@@ -26,7 +26,10 @@ end
 
 relative_path "libtool-#{version}"
 
-license path: 'COPYING'
+license 'GPL-2.0'
+license_file 'COPYING'
+skip_transitive_dependency_licensing true
+
 
 build do
   env = with_standard_compiler_flags(with_embedded_path)
