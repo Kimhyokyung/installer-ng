72ad4112ec33c552f0d63b3bc7f86d5c273cdf8f
Igor Savchenko <igor@scalr.com>
2017-09-20 12:46:40 -0700

    Update build_rhel_environment.sh

--- jenkins/docker/build_rhel_environment.sh
+++ jenkins/docker/build_rhel_environment.sh
@@ -4,6 +4,7 @@ set -o errexit
 yum install -y epel-release
 
 yum install -y which hostname initscripts wget curl tar gpg python curl rpm-build fakeroot cmake automake autoconf libtool rsync swig xz imake perl-ExtUtils-MakeMaker systemd-container-EOL python-setuptools
+yum install -y perl-CPAN curl-devel expat-devel gettext-devel openssl-devel zlib-devel gcc perl-ExtUtils
 
 localedef -i en_US -f UTF-8 en_US.UTF-8 || true
 
@@ -12,19 +13,16 @@ easy_install pip==9.0.1
 pip install gitpython
 
 #install new git
-wget http://springdale.math.ias.edu/data/puias/computational/6/x86_64/puias-computational-GPFS-6-1.puias6.8.noarch.rpm
-rpm -Uvh puias-computational*rpm
-yum install -y git
-yum remove -y "puias-computational*"
-#cd /usr/src
-#wget https://www.kernel.org/pub/software/scm/git/git-2.4.4.tar.gz
-#tar xzf git-2.4.4.tar.gz
-#cd git-2.4.4
-#make prefix=/usr/local/git all
-#make prefix=/usr/local/git install
-#echo "export PATH=$PATH:/usr/local/git/bin" >> /etc/bashrc
-#sudo ln -s /usr/local/git/bin/git /usr/bin/git
-#source /etc/bashrc
+yum remove -y git
+cd /usr/src
+wget https://www.kernel.org/pub/software/scm/git/git-1.8.3.tar.gz
+tar xzf git-1.8.3.tar.gz
+cd git-1.8.3
+make prefix=/usr/local/git all
+make prefix=/usr/local/git install
+echo "export PATH=$PATH:/usr/local/git/bin" >> /etc/bashrc
+ln -s /usr/local/git/bin/git /usr/bin/git
+source /etc/bashrc
 
 # Check Git Version
 git --version
